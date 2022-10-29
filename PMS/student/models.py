34199892dfaeeb64bbb97563a609b5d444aaced7
mohammadsohail17 <sohail23264@gmail.com>
2022-10-29 23:02:54 +0530

    models

--- PMS/student/models.py
+++ PMS/student/models.py
@@ -1,3 +1,23 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    roll_no = models.BigIntegerField()
+    gpa = models.FloatField()
+    course = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_no = models.CharField(max_length=10)
+    cutoff = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.name
+
--- PMS/student/views.py
+++ PMS/student/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from telnetlib import AUTHENTICATION
+from urllib import request
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,logout
 # Create your views here.
+def index(request):
+    if request.user.is_anonymous:
+        return redirect('/login')
+def login(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            return redirect('/')
+        else:
+            return render(request,'index.html')
+    return render(request,'login.html')
+def logout_view(request):
+    logout(request)
+    return redirect('/login')
+    
