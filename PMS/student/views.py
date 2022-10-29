e4ef85686bb5c71a00e50dce47ee33ea4acfb1aa
mohammadsohail17 <sohail23264@gmail.com>
2022-10-29 23:37:04 +0530

    c

--- PMS/student/views.py
+++ PMS/student/views.py
@@ -3,10 +3,28 @@ from urllib import request
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,logout
+from django.http import HttpResponse
+from .models import *
 # Create your views here.
+def home(request):
+    if request.POST :
+        sname = request.POST['name']
+        mail = request.POST['email']
+        gpa = request.POST['gpa']
+        course = request.POST['course']
+        temp = []
+        companies = Company.objects.all()
+        for i in range(len(companies)):
+            # the student who has more gpa than cutoff will be shortlisted and that 
+            # companies names are added to temp variable
+            if companies[i].cutoff <= gpa:
+                temp.append(companies[i].name)
+        #returning the companies he got shorlisted.
+        return render('index.html', {'shortlist':temp})
 def index(request):
-    if request.user.is_anonymous:
-        return redirect('/login')
+    '''if request.user.is_anonymous:
+        return redirect('/login')'''
+    return render(request,'login.html')
 def login(request):
     if request.method=="POST":
         username=request.POST.get('username')
