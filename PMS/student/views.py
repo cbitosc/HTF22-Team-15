045a30cd70bf118d2dee64481aae9e2b3dfdc03a
Shreya <kokashreya@gmail.com>
2022-10-30 00:59:49 +0530

    'commit'

--- PMS/student/views.py
+++ PMS/student/views.py
@@ -1,8 +1,10 @@
 from telnetlib import AUTHENTICATION
 from urllib import request
 from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,logout
+from django.contrib import messages
+from django.contrib.auth import authenticate,logout,login
 from django.http import HttpResponse
 from .models import *
 # Create your views here.
@@ -27,13 +29,19 @@ def index(request):
     return render(request,'login.html')
 def login(request):
     if request.method=="POST":
-        username=request.POST.get('username')
-        password=request.POST.get('password')
-        user=authenticate(username=username,password=password)
-        if user is not None:
-            return redirect('/')
+        form=AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user=authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password")
         else:
-            return render(request,'login.html')
+            messages.error(request, "Invalid username or password")
+    form=AuthenticationForm()
     return render(request,'login.html')
 def logout_view(request):
     logout(request)
